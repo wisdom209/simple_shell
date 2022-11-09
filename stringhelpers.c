8b0bc1456e9f23675ed1236b849b2586a1de480b
Oluwatobi <amure387@gmail.com>
2022-11-09 16:16:05 +0000

    strings

---
+++ stringhelpers.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strcat - appends
+ * @dest: first
+ * @src: second
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcmp - compare
+ * @s1: first
+ * @s2: second
+ * Return: number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
